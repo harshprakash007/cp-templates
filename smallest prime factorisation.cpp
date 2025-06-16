ff6f59d8a3597104893c6ef5ce75adf2f3a858e1
Harsh Prakash <214019244+harshprakash007@users.noreply.github.com>
2025-06-16 23:28:00 +0530

    Add files via upload

---
+++ smallest prime factorisation.cpp
@@ -0,0 +1,19 @@
+const ll n = 1e7;
+
+vector<ll> spf(n+1);
+
+for(int i = 0; i <= n; i++)
+{
+    spf[i] = i;
+}
+
+for(int i = 2; i*i<=n; i++)
+{
+    if(spf[i]==i)
+    {
+        for(int j = i*i; j<=n; j+=i)
+        {
+            if(spf[j]==j){spf[j]=i;}
+        }
+    }
+}
